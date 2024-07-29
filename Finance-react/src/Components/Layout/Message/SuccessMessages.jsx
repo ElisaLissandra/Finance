59b8f957086e9e9c079e9c29b0dd4a9fd011a34f
ElisaLissandra <elisa_lissandra@outlook.com>
2024-07-29 20:26:09 -0300

    Layout da mensagem

--- Finance-react/src/Components/Layout/Message/SuccessMessages.jsx
+++ Finance-react/src/Components/Layout/Message/SuccessMessages.jsx
@@ -4,7 +4,7 @@ import styles from "./Message.module.css";
 
 export default function SuccessMessages({message}) {
   return (
-    <div className={styles.message}>
+    <div className={styles.message}  style={{ display: message ? 'block' : 'none' }}>
       <p className={styles.message_success}>
         {message}
       </p>
